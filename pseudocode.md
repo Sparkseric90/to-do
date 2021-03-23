3703d5d610eca6607205efe24b818bd6a68c1668
EricSparks <sparkseric90@gmail.com>
2021-03-23 16:00:49 -0400

    started laying out app

---
+++ pseudocode.md
@@ -0,0 +1,18 @@
+As a user with a To-Do list I want to be able to:
+    - Add items to a list to remind the user of what they need to do.
+    - Remove items as you complete them.
+    - switch between views of All items, Active items, Completed Items.(Stretch)
+
+
+Output box.
+If user inputs data and hits enter or clicks a add button
+    then add data to list.
+
+
+List of items
+If user has inputted data and it has been added to the list
+    then options of Completing and deleting task becomes available. (Stretch)
+If user completes task on item, and completed button is used, item is then taken to the completed state.(stretch)
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '', taskList: [] };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+    console.log(this.state.value)
+  }
+  handleSubmit(event){
+    this.setState({taskList: [this.state.value]})
+    this.setState({value:''})
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div className="App container">
+        <div className="card-body text-center">
+          <blockquote className="blockquote mb-0" />
+          <p>My Todo List</p>
+        </div>
+      <form onSubmit={this.handleSubmit}>
+        <div className="input-group mb-3 mt-3">
+          <input type="text" value={this.state.value} onChange={this.handleChange} className="form-control" placeholder="What do you need to do?" aria-label="Recipient's username" aria-describedby="button-addon2" />
+          <button className="btn btn-outline-secondary" type="button" value="Submit" id="button-addon2">Submit</button>
+        </div>
+      </form>
+        <Todo word={this.state.taskList[0]}/>
+      </div>
+    );
+  }
 }
 
-export default App;
+  export default App;
